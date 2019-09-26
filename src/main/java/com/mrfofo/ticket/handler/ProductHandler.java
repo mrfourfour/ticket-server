b4d28117eb36033099c678e77250d55eecb62509
aerain <j40f893@gmail.com>
2019-09-26 10:18:08 +0900

    상품 개발

--- src/main/java/com/mrfofo/ticket/handler/ProductHandler.java
+++ src/main/java/com/mrfofo/ticket/handler/ProductHandler.java
@@ -1,13 +1,29 @@
 package com.mrfofo.ticket.handler;
 
+import com.mrfofo.ticket.model.Product;
+import com.mrfofo.ticket.repository.DynamoDbRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+
+
 @Component
+@RequiredArgsConstructor
 public class ProductHandler {
+
+    private final DynamoDbRepository<Product, String> repository;
+
     public Mono<ServerResponse> findByCategory(ServerRequest serverRequest) {
         return null;
+    }
+
+    public Mono<ServerResponse> findAll(ServerRequest serverRequest) {
+        return repository.findAll().collectList().flatMap(products -> ServerResponse.ok().body(fromObject(Map.of("data", products))));
     }
 }
--- src/main/java/com/mrfofo/ticket/repository/ProductRepository.java
+++ src/main/java/com/mrfofo/ticket/repository/ProductRepository.java
@@ -1,4 +1,60 @@
 package com.mrfofo.ticket.repository;
 
-public class ProductRepository {
+import com.mrfofo.ticket.model.Product;
+import com.mrfofo.ticket.model.Ticket;
+import com.mrfofo.ticket.objectmapper.DynamoDbMapper;
+import com.mrfofo.ticket.objectmapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepository implements DynamoDbRepository<Product, String> {
+
+    private final DynamoDbMapper<Product> productMapper;
+    private final DynamoDbAsyncClient client;
+
+    @Override
+    public Flux<Product> findAll() {
+        CompletableFuture<QueryResponse> future = client.query(QueryRequest.builder()
+                .tableName("ticket")
+                .indexName("sortByDate")
+                .keyConditionExpression("PK = :pk")
+                .expressionAttributeValues(Map.of(":pk", AttributeValue.builder().s("Product").build()))
+                .scanIndexForward(false)
+                .build());
+
+        CompletableFuture<List<Product>> productListFuture = future
+                .thenApplyAsync(QueryResponse::items)
+                .thenApplyAsync(list -> list.parallelStream()
+                    .map(productMapper::toObj)
+                    .collect(Collectors.toList())
+                );
+
+        return Mono.fromFuture(productListFuture).flatMapMany(Flux::fromIterable);
+    }
+
+    @Override
+    public Mono<Product> findById(String id) {
+        return null;
+    }
+
+    @Override
+    public Mono<Product> save(Product product) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> delete() {
+        return null;
+    }
 }
